ab42f1e43c8c9df3fb6e00a3a2ab181f362a3495
Josemaria Bolanos <josemabol@gmail.com>
2025-02-28 15:15:16 -0600

    Refectored controller to make implementation easier

--- amd/src/generator.js
+++ amd/src/generator.js
@@ -1,9 +1,8 @@
 define([
     'jquery',
-    'core/ajax',
     'core/templates',
     'core/str'
-], function($, Ajax, Template, Str) {
+], function($, Template, Str) {
     const generatorForm         = document.getElementById('edai_course_generator_form');
     const promptContainer       = generatorForm.querySelector('.prompt-container');
     const promptForm            = generatorForm.querySelector('#prompt-form');
@@ -14,13 +13,28 @@ define([
     const courseFiles           = generatorForm.querySelector('#course_files');
     const filesContainer        = generatorForm.querySelector('#file_names');
     const spinner               = generatorForm.querySelector('#progress-spinner');
-    const loader                = generatorForm.querySelector('#loader');
-    const successContainer      = generatorForm.querySelector('#success_message_container');
 
     return {
         init: function() {
             this.progress = 0;
 
+            this.adjustDescriptionHeight();
+            this.handleDragAndDrop();
+
+            // Add event listener to generate course button.
+            generateCourse.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (this.progress === 0) {
+                    this.startProgress();
+                } else if (this.progress < 100) {
+                    let courseid = 2; // Replace with actual course id.
+                    this.finishProgress(courseid);
+                } else {
+                    this.resetProgress();
+                }
+            });
+        },
+        adjustDescriptionHeight: function() {
             // Adjust course description height.
             const initialheight = courseDescription.clientHeight;
             courseDescription.addEventListener('input', function() {
@@ -42,57 +56,6 @@ define([
                 // Additional padding.
                 newheight += newheight > initialheight ? 14 : 0;
                 courseDescription.style.height = newheight + 'px';
-            });
-
-            // Prevent default action and start progress.
-            generateCourse.addEventListener('click', (event) => {
-                event.preventDefault();
-                loader.classList.replace('d-none', 'd-block');
-                if (this.progress === 0) {
-                    // Begin progress animation.
-                    this.startProgress(loader);
-                    spinner.classList.add('spinner-border');
-                    generationContainer.classList.replace('d-none', 'd-block');
-                } else if (this.progress < 100) {
-                    // Finish progress.
-                    this.setProgress(loader, 100);
-                    successContainer.classList.replace('d-none', 'd-block');
-                    spinner.classList.remove('spinner-border');
-                } else {
-                    // Reset progress.
-                    this.setProgress(loader, 0);
-                    successContainer.classList.replace('d-block', 'd-none');
-                    spinner.classList.remove('spinner-border');
-                    generationContainer.classList.replace('d-block', 'd-none');
-                    this.clearAllFiles();
-                }
-            });
-
-            // Drag over prompt input.
-            dragEnterCounter = 0;
-            $('#prompt-form').bind({
-                dragenter: function(event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    dragEnterCounter++;
-                    promptContainer.classList.add('drag-over');
-                },
-                dragleave: function(event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    dragEnterCounter--;
-                    if (dragEnterCounter === 0) { 
-                        promptContainer.classList.remove('drag-over');
-                    }
-                },
-            });
-
-            // Apply drop listeners to all child elements of prompt form.
-            this.dropOnChildElements(promptForm);
-            // Move files from temp to course files.
-            tempCourseFiles.addEventListener('change', () => {
-                let newFiles = Array.from(tempCourseFiles.files);
-                this.transferFiles(newFiles);
             });
         },
         clearAllFiles: function() {
@@ -119,6 +82,34 @@ define([
             courseFiles.files = dataTransfer.files;
             this.displayFileNames();
         },
+        handleDragAndDrop: function() {
+            // Drag over and leave over prompt form.
+            let dragEnterCounter = 0;
+            $('#prompt-form').bind({
+                dragenter: function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    dragEnterCounter++;
+                    promptContainer.classList.add('drag-over');
+                },
+                dragleave: function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    dragEnterCounter--;
+                    if (dragEnterCounter === 0) { 
+                        promptContainer.classList.remove('drag-over');
+                    }
+                },
+            });
+
+            // Apply drop listeners to all child elements of prompt form.
+            this.dropOnChildElements(promptForm);
+            // Move files from temp to course files.
+            tempCourseFiles.addEventListener('change', () => {
+                let newFiles = Array.from(tempCourseFiles.files);
+                this.transferFiles(newFiles);
+            });
+        },
         dropOnChildElements: function(node) {
             node.childNodes.forEach(child => {
                 this.dropOnChildElements(child);
@@ -139,7 +130,10 @@ define([
                 });
             });
         },
-        startProgress: function(loader) {
+        startProgress: function() {
+            spinner.classList.add('spinner-border');
+            generationContainer.classList.replace('d-none', 'd-block');
+
             let interval = setInterval(() => {
                 if (this.progress >= 90) {
                     clearInterval(interval);
@@ -147,18 +141,43 @@ define([
 
                 // Increase by a random amount every second
                 let increment = Math.floor(Math.random() * 5);
-                this.setProgress(loader, this.progress + increment);
+                this.setProgress(this.progress + increment);
             }, 1000);
         },
-        setProgress: function(loader, progress) {
-            let progressBar = loader.querySelector('.s-progress--bar');
+        finishProgress: async function(courseid) {
+            spinner.classList.remove('spinner-border');
+            
+            Template.render('block_course_generator/success_message', {courseid: courseid}).then((html) => {
+                generationContainer.innerHTML += html;
+            }).catch((error) => {
+                console.error("Error rendering template: ", error);
+            });
+
+            this.setProgress(100);
+        },
+        resetProgress: function() {
+            spinner.classList.remove('spinner-border');
+            generationContainer.classList.replace('d-block', 'd-none');
+
+            let successContainer = generatorForm.querySelector('#success_message_container');
+            if (successContainer) {
+                successContainer.remove();
+            }
+
+            this.clearAllFiles();
+
+            this.setProgress(0);
+        },
+        setProgress: function(progress) {
+            this.progress = progress;
+
+            let progressBar = generatorForm.querySelector('.s-progress--bar');
             progressBar.style.width = `${progress}%`;
             if (progress >= 100) {
                 progressBar.classList.add('done');
             } else {
                 progressBar.classList.remove('done');
             }
-            this.progress = progress;
         },
         displayFileNames: function() {
             let contextFiles = [];
@@ -186,6 +205,10 @@ define([
                     let toDelete = courseFiles.files[index].name;
 
                     deleteIcon.addEventListener('click', function() {
+                        // Remove file from display.
+                        let toolTipId = deleteIcon.getAttribute('aria-describedby');
+                        document.getElementById(toolTipId).remove();
+
                         // Remove file from course files.
                         let dataTransfer = new DataTransfer();
                         for (let i = 0; i < courseFiles.files.length; i++) {
@@ -195,12 +218,7 @@ define([
                         }
                     
                         courseFiles.files = dataTransfer.files;
-                        
-                        // Remove file from display and update total.
-                        let toolTipId = deleteIcon.getAttribute('aria-describedby');
-                        document.getElementById(toolTipId).remove();
-                        this.closest('div').remove();
-                        that.updateTotalSize();
+                        that.displayFileNames();
                     });
                 });
 
@@ -216,23 +234,6 @@ define([
                 unitIndex++;
             }
             return `${size.toFixed(1)} ${units[unitIndex]}`;
-        },
-        updateTotalSize: function() {
-            let totalSize = 0;
-            for (let i = 0; i < courseFiles.files.length; i++) {
-                const file = courseFiles.files[i];
-                totalSize += file.size;
-            };
-
-            let totalSizeElement = filesContainer.querySelector('.total-size');
-            if (totalSize > 0) {
-                Str.get_string('totalsize', 'block_course_generator', this.formatFilesize(totalSize))
-                .done(function(message) {
-                    totalSizeElement.innerHTML = message;
-                });
-            } else {
-                totalSizeElement.innerHTML = '';
-            }
         }
     };
 });
