83bfae4f7c83e48ab9381117c5d9240a6bcc61b9
Josemaria Bolanos <josemabol@gmail.com>
2025-09-10 10:41:52 -0400

    Add Missing Header and Copyright Information in JS and Mustache Files

--- amd/src/dirty.js
+++ amd/src/dirty.js
@@ -1,4 +1,26 @@
-// amd/src/admindirty.js
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * AMD module for settings UI.
+ *
+ * @module     block_dixeo_coursegen/dirty
+ * @author     Josemaria Bolanos <admin@mako.digital>
+ * @copyright  2025 Dixeo (contact@dixeo.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 define(['jquery'], function($) {
     return {
         init: function() {
